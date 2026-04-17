dbacadc93b9064e13adc55675e0f9d27e5e9285e
Park Eunsu <pgu1458@gmail.com>
2026-04-18 04:02:48 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace KiosK
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            // 이 설정이 창 크기가 멋대로 커지는 것을 막아줍니다.
+            if (Environment.OSVersion.Version.Major >= 6)
+                Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form2());
+        }
+    }
+}
